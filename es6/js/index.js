dde953cb863ca8c66c08208afcf3f2e9d4b60c75
Joel Ross <joelross@uw.edu>
2025-04-30 15:13:35 -0700

    End of es6 demo

--- es6/js/index.js
+++ es6/js/index.js
@@ -1,6 +1,35 @@
 'use strict';
 
-//console.log("executing main.js")
+import { getOtherPeople } from './other.js';
+import getBMI from './other.js';
+
+
+console.log("executing main.js")
+
+// const foo = (params) => {
+//   return 'foo '+params;
+// }
+// const sayHello = () => onsole.log("Hello world");
+
+// const myArray = [1, 2, 3];
+// const [x, y, z, a] = myArray;
+// // const x = myArray[0];
+// // const y = myArray[1];
+// // const z = myArray[2];
+
+// console.log('x', x);
+// console.log('y', y);
+// console.log('z', z);
+// console.log('a', a);
+
+// const myObject = {a: 1, b: 2, c: 3};
+// const {b, a, c, d} = myObject; //myObject.a to a, etc.
+
+// console.log(a); //=> 1
+// console.log(b); //=> 2;
+// console.log(c); //=> 3;
+// console.log(d);
+
 
 //an array of objects to work with
 const peopleArray = [
@@ -11,3 +40,26 @@ const peopleArray = [
   {name: 'Emma', height: 71, weight: 152}
 ]
 //console.log(peopleArray);
+
+const peopleAndFriendsArray = [
+  ...peopleArray,
+  {name: 'Fred', height: 65, weight: 140}
+]
+
+console.log(peopleAndFriendsArray);
+
+const originalArray = ['a', 'b', 'c', 'd'];
+const newArray = ['z', ...originalArray, 'e', 'f'];
+console.log(newArray);
+const arrayCopy = [...originalArray];
+console.log(arrayCopy);
+
+const person = {name: 'Ada', height: 64, weight: 135}
+
+const copyOfPerson = {height: 66, ...person}; //clone an object!
+console.log(copyOfPerson);
+
+const otherPeople = getOtherPeople();
+console.log(otherPeople);
+
+console.log(getBMI(peopleArray[0]));
--- es6/js/other.js
+++ es6/js/other.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log("executing other.js");
+
+const peopleArray = ['fred', 'steve', 'other'];
+console.log(peopleArray);
+
+export function getOtherPeople() {
+  return peopleArray;
+}
+
+export default function getBMI({height, weight}) { 
+  return 703*weight/(height*height);
+}
