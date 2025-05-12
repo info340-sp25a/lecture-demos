00190e10b21c790e22f8cdbdadbe9287ddc06cee
Joel Ross <joelross@uw.edu>
2025-05-12 08:35:15 -0700

    Add react interactivity demo starter code

---
+++ react-chat-2/src/components/ComposeForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function ComposeForm(props) {
+  const { currentChannel } = props;
+
+  return (
+    <form className="my-2">
+      <div className="input-group">
+        <textarea 
+          className="form-control" rows="2" placeholder="Type a new message"
+        />
+        <button type="submit" className="btn btn-secondary">
+          <span className="material-icons">send</span>
+        </button>
+      </div>
+    </form>
+  );
+}
